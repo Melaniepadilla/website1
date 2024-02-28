a71466d17e010e79f10d07449f4127ec59425853
Melanie Padilla <142541196+Melaniepadilla@users.noreply.github.com>
2024-02-28 17:56:14 +0000

    Wed, Feb 28, 2024, 9:56 AM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -1,7 +1,15 @@
 panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
-    panel.addEventlistener('click', () => {
+    panel.addEventListener('click', () => {
         panel.classList.add('active')
     })
 })
+
+
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
