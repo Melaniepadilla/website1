7626b0ce4788db6d618eaed49c63ad003a49dc75
Melanie Padilla <142541196+Melaniepadilla@users.noreply.github.com>
2024-03-06 17:06:07 +0000

    Wed, Mar 6, 2024, 9:06 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -3,5 +3,11 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    console.log(window.scollY)
+    console.log(window.scrollY)
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    }
+    else {
+        nav.classList.remove('active')
+    }
 }
